509efa0083f864c99696d0eea51acdb379ca731d
SystemM1000 <gustaf_alfredo@outlook.com>
2023-09-10 19:11:57 -0300

    Backend completed

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -2,11 +2,38 @@ package com.example.cardapio.controller;
 
 import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
+@RestController // Indicar que essa classe é um controller
+@RequestMapping("food") // Indicar o endpoint que o controller controla
+public class FoodController {
+    @Autowired // Anotação para o Spring injetar uma dependência
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*") // Permitir todas as origens e todos os headers
+    @PostMapping // Mapea para o Spring a classe responsavel de lidar com as requisições do tipo post
+    public void saveFood(@RequestBody FoodRequestDTO data){ // Indicando para o Spring que o parametro é um Body que veio la do request do cliente
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping // Quando bater nesse endpoint food com o método get executa esse método da classe: getAll
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList(); // stream() -> esse método vai pegar tudo que está vindo do repository com o método findAll() e vai colocar dentro de um funil
+        return foodList;
+    }
+}
+
+/*
+
+// Não é uma boa prática retornar o objeto food como uma lista
+// O certo seria criar uma nova classe que vai ser o DTO(data transfer object)
 
 @RestController // Indicar que essa classe é um controller
 @RequestMapping("food") // Indicar o endpoint que o controller controla
@@ -19,3 +46,4 @@ public class FoodController {
         return foodList;
     }
 }
+ */
--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -1,14 +1,28 @@
 package com.example.cardapio.food;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 // Classe indica uma entidade do nosso banco de dados
 @Table(name = "foods")  // Nome da tabela
 @Entity(name = "foods") // Nome da entidade
-public class Food {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Getter // solicita para o Lombok em runtime gerar todos os métodos de get
+@NoArgsConstructor // Lombok declara um construtor que recebe nenhum argumento
+@AllArgsConstructor // Lombok declara um construtor que recebe todos os argumentos
+@EqualsAndHashCode(of = "id") // Representa que o "id" é a representação única de Food
+public class Food { // Com isso o id gerado para os elementos será em ordem númerica e não aleatoriamente
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)  // Recomendado usar UUID para aplicações reais
     private Long id;
     private String title;
     private String image;
     private Integer price;
+
+    public Food(FoodRequestDTO data) {
+        this.image = data.image();
+        this.price = data.price();
+        this.title = data.title();
+    }
 }
